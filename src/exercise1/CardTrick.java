a06177308c2fcc53fa1555c4d60922da335a15ba
alfinjohnsonn <johnsalf@sheridancollege.ca>
2023-01-25 23:05:33 -0500

    Card Trick Ver 1.3

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,4 +1,5 @@
 package exercise1;
+import java.util.*;
 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
@@ -7,28 +8,66 @@ package exercise1;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 25, 2022 
+ * editied by Alfin J
+ * This is a branch
  */
 public class CardTrick {
     
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         
         Card[] hand = new Card[7];
 
-        for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
+        for (int i = 0; i < hand.length; i++)
+        {
+            Card Randomcard = new Card();
+            Randomcard.setValue((int)(1+Math.random()*13));
             //card.setValue(insert call to random number generator here)
-            // 
+            
+            Randomcard.setSuit(Card.SUITS[(int)(0+Math.random()*3)]);
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            hand[i] = Randomcard;
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
+            System.out.println(hand[i].getValue()+" "+hand[i].getSuit());
             //       Don't worry about duplicates at this point
         }
-
+        
+        Card UserCard = new Card();
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter a number from 1-13(11 for Jack,12 for Queen, 13 for King) : ");
+        UserCard.setValue(inp.nextInt());
+        
+        Scanner inp2 = new Scanner(System.in);
+        System.out.println("Enter a for Card Suit (Hearts, Diamonds, Spades, Clubs) !Caution:Enter the names as given in the line! : ");
+        UserCard.setSuit(inp2.nextLine());
+        
+        System.out.println(UserCard.getValue()+ " " +UserCard.getSuit());//this line print the users card
+        
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
+        //11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
+        // 1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
         // 
+        
+        boolean a = false;
+        for(int i = 0; i< hand.length; i++)// this for loop checks the user inputed card to the random generated card 
+        {
+            if(UserCard.getValue()== hand[i].getValue() && UserCard.getSuit().equals(hand[i].getSuit())){
+                a = true;
+            }
+            else continue;
+        }
+        
+        if(a)// if a match  is found it will do this validation
+        {
+            System.out.println("Match found");
+            printInfo();
+        }
+        else
+        {
+            System.out.println("No Match");//no match is found this validation is used
+        }
         // Then loop through the cards in the array to see if there's a match.
         
         // If the guess is successful, invoke the printInfo() method below.
@@ -65,3 +104,5 @@ public class CardTrick {
     }
 
 }
+
+
